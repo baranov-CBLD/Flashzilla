59bc8340a78cd903454d86b21d9b007868fc1971
Kirill <K-baranov@outlook.com>
2024-03-07 11:28:54 +0100

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Kirill Baranov on 07/03/24.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
